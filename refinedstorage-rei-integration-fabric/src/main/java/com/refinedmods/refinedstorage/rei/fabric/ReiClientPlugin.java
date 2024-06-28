c470bd5a12c298442b8bde6ba4c211381ea07f87
raoulvdberge <raoulvdberge@gmail.com>
2024-06-28 09:15:28 +0200

    chore: update refined storage

--- refinedstorage-rei-integration-fabric/src/main/java/com/refinedmods/refinedstorage/rei/fabric/ReiClientPlugin.java
+++ refinedstorage-rei-integration-fabric/src/main/java/com/refinedmods/refinedstorage/rei/fabric/ReiClientPlugin.java
@@ -2,8 +2,6 @@ package com.refinedmods.refinedstorage.rei.fabric;
 
 import java.util.function.Supplier;
 
-import com.refinedmods.refinedstorage2.platform.common.content.BlockColorMap;
-import com.refinedmods.refinedstorage2.platform.common.content.Blocks;
 import com.refinedmods.refinedstorage2.platform.common.content.ContentIds;
 import com.refinedmods.refinedstorage2.platform.common.content.Items;
 import com.refinedmods.refinedstorage2.platform.common.content.Tags;
@@ -19,12 +17,14 @@ import me.shedaniel.rei.api.common.util.EntryIngredients;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
 import static com.refinedmods.refinedstorage.rei.common.Common.FULLY_CHARGED_CONTROLLER_GROUP_ID;
 import static com.refinedmods.refinedstorage.rei.common.Common.FULLY_CHARGED_CONTROLLER_GROUP_NAME;
+import static com.refinedmods.refinedstorage2.platform.common.util.IdentifierUtil.createTranslation;
 
 @Environment(EnvType.CLIENT)
 public class ReiClientPlugin implements REIClientPlugin {
@@ -45,24 +45,27 @@ public class ReiClientPlugin implements REIClientPlugin {
         Items.INSTANCE.getControllers().stream().map(Supplier::get).forEach(registry::registerNbt);
     }
 
+    private Component tagName(final String name) {
+        return createTranslation("tag.item", name);
+    }
+
     @SuppressWarnings("UnstableApiUsage")
     @Override
     public void registerCollapsibleEntries(final CollapsibleEntryRegistry registry) {
-        groupItems(registry, Blocks.INSTANCE.getCable(), ContentIds.CABLE, Tags.CABLES);
-        groupItems(registry, Blocks.INSTANCE.getGrid(), ContentIds.GRID, Tags.GRIDS);
-        groupItems(registry, Blocks.INSTANCE.getCraftingGrid(), ContentIds.CRAFTING_GRID, Tags.CRAFTING_GRIDS);
-        groupItems(registry, Blocks.INSTANCE.getImporter(), ContentIds.IMPORTER, Tags.IMPORTERS);
-        groupItems(registry, Blocks.INSTANCE.getExporter(), ContentIds.EXPORTER, Tags.EXPORTERS);
-        groupItems(registry, Blocks.INSTANCE.getDetector(), ContentIds.DETECTOR, Tags.DETECTORS);
-        groupItems(registry, Blocks.INSTANCE.getDestructor(), ContentIds.DESTRUCTOR, Tags.DESTRUCTORS);
-        groupItems(registry, Blocks.INSTANCE.getConstructor(), ContentIds.CONSTRUCTOR, Tags.CONSTRUCTORS);
-        groupItems(registry, Blocks.INSTANCE.getExternalStorage(), ContentIds.EXTERNAL_STORAGE, Tags.EXTERNAL_STORAGES);
-        groupItems(registry, Blocks.INSTANCE.getController(), ContentIds.CONTROLLER, Tags.CONTROLLERS);
+        groupItems(registry, tagName("cables"), ContentIds.CABLE, Tags.CABLES);
+        groupItems(registry, tagName("grids"), ContentIds.GRID, Tags.GRIDS);
+        groupItems(registry, tagName("crafting_grids"), ContentIds.CRAFTING_GRID, Tags.CRAFTING_GRIDS);
+        groupItems(registry, tagName("importers"), ContentIds.IMPORTER, Tags.IMPORTERS);
+        groupItems(registry, tagName("exporters"), ContentIds.EXPORTER, Tags.EXPORTERS);
+        groupItems(registry, tagName("detectors"), ContentIds.DETECTOR, Tags.DETECTORS);
+        groupItems(registry, tagName("destructors"), ContentIds.DESTRUCTOR, Tags.DESTRUCTORS);
+        groupItems(registry, tagName("constructors"), ContentIds.CONSTRUCTOR, Tags.CONSTRUCTORS);
+        groupItems(registry, tagName("external_storages"), ContentIds.EXTERNAL_STORAGE, Tags.EXTERNAL_STORAGES);
+        groupItems(registry, tagName("controllers"), ContentIds.CONTROLLER, Tags.CONTROLLERS);
         registry.group(
             FULLY_CHARGED_CONTROLLER_GROUP_ID,
             FULLY_CHARGED_CONTROLLER_GROUP_NAME,
-            Items.INSTANCE.getControllers()
-                .stream()
+            Items.INSTANCE.getControllers().stream()
                 .map(Supplier::get)
                 .map(ControllerBlockItem::createAtEnergyCapacity)
                 .map(EntryStacks::of)
@@ -70,34 +73,40 @@ public class ReiClientPlugin implements REIClientPlugin {
         );
         groupItems(
             registry,
-            Blocks.INSTANCE.getCreativeController(),
+            tagName("creative_controllers"),
             ContentIds.CREATIVE_CONTROLLER,
             Tags.CREATIVE_CONTROLLERS
         );
         groupItems(
             registry,
-            Blocks.INSTANCE.getWirelessTransmitter(),
+            tagName("wireless_transmitters"),
             ContentIds.WIRELESS_TRANSMITTER,
             Tags.WIRELESS_TRANSMITTERS
         );
-        groupItems(registry, Blocks.INSTANCE.getNetworkReceiver(), ContentIds.NETWORK_RECEIVER, Tags.NETWORK_RECEIVERS);
+        groupItems(registry, tagName("network_receivers"), ContentIds.NETWORK_RECEIVER, Tags.NETWORK_RECEIVERS);
         groupItems(
             registry,
-            Blocks.INSTANCE.getNetworkTransmitter(),
+            tagName("network_transmitters"),
             ContentIds.NETWORK_TRANSMITTER,
             Tags.NETWORK_TRANSMITTERS
         );
         groupItems(
             registry,
-            Blocks.INSTANCE.getSecurityManager(),
+            tagName("security_managers"),
             ContentIds.SECURITY_MANAGER,
             Tags.SECURITY_MANAGERS
         );
         groupItems(
             registry,
-            Blocks.INSTANCE.getRelay(),
+            tagName("relays"),
             ContentIds.RELAY,
             Tags.RELAYS
+        );
+        groupItems(
+            registry,
+            tagName("disk_interfaces"),
+            ContentIds.DISK_INTERFACE,
+            Tags.DISK_INTERFACES
         );
     }
 
@@ -109,14 +118,10 @@ public class ReiClientPlugin implements REIClientPlugin {
     @SuppressWarnings("UnstableApiUsage")
     private static void groupItems(
         final CollapsibleEntryRegistry registry,
-        final BlockColorMap<?, ?> blocks,
+        final Component name,
         final ResourceLocation itemIdentifier,
         final TagKey<Item> tag
     ) {
-        registry.group(
-            itemIdentifier,
-            blocks.getDefault().getName(),
-            EntryIngredients.ofItemTag(tag)
-        );
+        registry.group(itemIdentifier, name, EntryIngredients.ofItemTag(tag));
     }
 }
